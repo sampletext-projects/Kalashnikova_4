72466b48b3c95dd13968bcbc9741b593b001edcd
sampletext32 <sampletext32@bk.ru>
2020-06-25 19:52:35 +0300

    Add project

---
+++ Kalashnikova_4/main.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "russian");
+
+	cout << "Программу сделала Калашникова А. М. УМЛ - 112\n";
+	cout << "Программа для функции\n";
+
+	double x = 0.7;
+
+	for (int n = 5; n <= 25; n += 5)
+	{
+		double y = 1;
+		int sign = -1;
+		int fact = 1;
+		double x_n = x;
+
+		for (int i = 0; i < n; i++)
+		{
+			y += sign * x_n / fact;
+			sign *= -1;
+			fact *= i + 2;
+			x_n *= x;
+		}
+
+		cout << "Y(" << x << ") [n: " << n << "] = " << y << "\n";
+	}
+
+	system("pause");
+
+	return 0;
+}
